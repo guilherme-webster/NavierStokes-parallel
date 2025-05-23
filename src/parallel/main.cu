0172fe8b79b2c40c7d8bbe1f2154151de5c11aa1
Jmvjr <j219255@dac.unicamp.br>
2025-05-23 20:55:44 -0300

    test

--- src/parallel/main.cu
+++ src/parallel/main.cu
@@ -26,12 +26,113 @@ typedef struct {
     int j;
     int side;
 } BoundaryPoint;
+
+// arrays iniciadas em 0
+__device__ double* d_F, *d_G;
+__device__ double* d_RHS, *d_res;
+__device__ double* d_u, *d_v, *d_p;
+
+// variaveis do device
+__device__ double d_delta_t, d_delta_x, d_delta_y, d_gamma;
+__device__ double du_max, dv_max;
+
+// variaveis tiradas do host (device pointers)
+__device__ int* d_i_max, *d_j_max;
+__device__ double* d_tau, *d_Re;
+__device__ BoundaryPoint* d_boundary_indices;
+__device__ double* d_gx, *d_gy;
+__device__ double* d_omega, *d_epsilon;
+__device__ int* d_max_it;
+// variables for norms
+double* d_norm_p, *d_norm_res;
+
+
+void init_memory(int i_max, int j_max,  BoundaryPoint* h_boundary_indices, int total_points, double* tau, double* Re, double* g_x, double* g_y
+                , double* omega, double* epsilon, int* max_it);
+
+
+void free_memory_kernel();
+
+__global__ void pick_max();
+
+
+
+double orchestration(int i_max, int j_max);
+    
+
+__global__ void min_and_gamma ();
+
+
+__device__ double atomicMax(double* address, double val);
+
+__device__ double atomicAddDouble(double* address, double val);
+
+
+__global__ void max_reduce_kernel(int i_max, int j_max, double* arr, double* max_val);
+
+BoundaryPoint* generate_boundary_indices(int i_max, int j_max, int* total_points);
+
+__global__ void update_boundaries_kernel();
+
+// Funções diferenças finitas com índices linearizados para GPU
+
+__device__ double du2_dx(double* u, double* v, int i, int j, double delta_x, double gamma, int j_max);
+
+__device__ double duv_dy(double* u, double* v, int i, int j, double delta_y, double gamma, int j_max);
+
+__device__ double dv2_dy(double* v, double* u, int i, int j, double delta_y, double gamma, int j_max);
+
+__device__ double duv_dx(double* u, double* v, int i, int j, double delta_x, double gamma, int j_max);
 /**
- * @brief Main function.
- * 
- * This is the main function.
- * @return 0 on exit.
+ * Central differences for second derivatives.
  */
+
+__device__ double d2u_dx2(double* u, int i, int j, double delta_x, int j_max) ;
+
+__device__ double d2u_dy2(double* u, int i, int j, double delta_y, int j_max) ;
+
+__device__ double d2v_dx2(double* v, int i, int j, double delta_x, int j_max) ;
+
+__device__ double d2v_dy2(double* v, int i, int j, double delta_y, int j_max) ;
+
+__global__ void calculate_F(double* F, double* u, double* v, int i_max, int j_max, double Re,
+    double g_x, double delta_t, double delta_x, double delta_y, double gamma) ;
+
+__global__ void calculate_G(double * G, double* u, double* v, int i_max, int j_max, double Re,
+    double g_y, double delta_t, double delta_x, double delta_y, double gamma) ;
+
+
+__global__ void calculate_RHS(double* RHS, double* F, double* G, double* u, double* v, int i_max, int j_max,
+    double delta_t, double delta_x, double delta_y) ;
+
+__global__ void red_kernel(double* p, double* RHS, double* u, double* v, int i_max, int j_max,
+    double delta_x, double delta_y, double omega) ;
+
+__global__ void black_kernel(double* p, double* RHS, double* u, double* v, int i_max, int j_max,
+    double delta_x, double delta_y, double omega) ;
+
+
+__global__ void calculate_ghost() ;
+
+
+__global__ void L2_norm(double* norm, double* m, int i_max, int j_max);
+
+
+__global__ void residual_kernel(double* res, double* p, double* RHS, int i_max, int j_max,
+    double delta_x, double delta_y) ;
+
+__global__ void update_velocity_kernel(double* u, double* v, double* p, int i_max, int j_max,
+    double delta_t, double delta_x, double delta_y);
+
+__global__ void extract_value_kernel(double* d_u, double* d_v, double* d_p, int i_max,int j_max, double* result);
+
+
+/**
+* @brief Main function.
+* 
+* This is the main function.
+* @return 0 on exit.
+*/
 
 int main(int argc, char* argv[])
 {
@@ -155,25 +256,8 @@ int main(int argc, char* argv[])
     } \
   } while(0)
 
-// arrays iniciadas em 0
-__device__ double* d_F, *d_G;
-__device__ double* d_RHS, *d_res;
-__device__ double* d_u, *d_v, *d_p;
 
-// variaveis do device
-__device__ double d_delta_t, d_delta_x, d_delta_y, d_gamma;
-__device__ double du_max, dv_max;
 
-// variaveis tiradas do host (device pointers)
-__device__ int* d_i_max, *d_j_max;
-__device__ double* d_tau, *d_Re;
-__device__ BoundaryPoint* d_boundary_indices;
-__device__ double* d_gx, *d_gy;
-__device__ double* d_omega, *d_epsilon;
-__device__ int* d_max_it;
-
-// variables for norms
-double* d_norm_p, *d_norm_res;
 
 void init_memory(int i_max, int j_max,  BoundaryPoint* h_boundary_indices, int total_points, double* tau, double* Re, double* g_x, double* g_y
                 , double* omega, double* epsilon, int* max_it) {
