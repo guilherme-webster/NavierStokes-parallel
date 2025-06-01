5d038d3fccaabbe623ba1e068eb514a016c32b48
Jmvjr <j219255@dac.unicamp.br>
2025-06-01 19:17:55 -0300

    new kernels

--- src/openmp/main.c
+++ src/openmp/main.c
@@ -16,10 +16,86 @@
 #include "integration.h"
 #include "boundaries.h"
 
-
 #include <time.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <omp.h>
+
+// Helper functions for GPU computation
+#pragma omp declare target
+inline double du2_dx_gpu(double* u, double* v, int i, int j, int j_max, double delta_x, double gamma) {
+    double stencil1 = 0.5 * (u[i*(j_max+2) + j] + u[(i+1)*(j_max+2) + j]);
+    double stencil2 = 0.5 * (u[(i-1)*(j_max+2) + j] + u[i*(j_max+2) + j]);
+
+    double stencil3 = fabs(stencil1) * 0.5 * (u[i*(j_max+2) + j] - u[(i+1)*(j_max+2) + j]);
+    double stencil4 = fabs(stencil2) * 0.5 * (u[(i-1)*(j_max+2) + j] - u[i*(j_max+2) + j]);
+
+    return 1/delta_x * (stencil1*stencil1 - stencil2*stencil2) + gamma / delta_x * (stencil3 - stencil4);
+}
+
+inline double duv_dy_gpu(double* u, double* v, int i, int j, int j_max, double delta_y, double gamma) {
+    double stencil1 = 0.5 * (v[i*(j_max+2) + j] + v[(i+1)*(j_max+2) + j]);
+    double stencil2 = 0.5 * (v[i*(j_max+2) + (j-1)] + v[(i+1)*(j_max+2) + (j-1)]);
+
+    double stencil3 = stencil1 * 0.5 * (u[i*(j_max+2) + j] + u[i*(j_max+2) + (j+1)]);
+    double stencil4 = stencil2 * 0.5 * (u[i*(j_max+2) + (j-1)] + u[i*(j_max+2) + j]);
+
+    double stencil5 = fabs(stencil1) * 0.5 * (u[i*(j_max+2) + j] - u[i*(j_max+2) + (j+1)]);
+    double stencil6 = fabs(stencil2) * 0.5 * (u[i*(j_max+2) + (j-1)] - u[i*(j_max+2) + j]);
+
+    return 1/delta_y * (stencil3 - stencil4) + gamma / delta_y * (stencil5 - stencil6);
+}
+
+inline double dv2_dy_gpu(double* u, double* v, int i, int j, int j_max, double delta_y, double gamma) {
+    double stencil1 = 0.5 * (v[i*(j_max+2) + j] + v[i*(j_max+2) + (j+1)]);
+    double stencil2 = 0.5 * (v[i*(j_max+2) + (j-1)] + v[i*(j_max+2) + j]);
+
+    double stencil3 = fabs(stencil1) * 0.5 * (v[i*(j_max+2) + j] - v[i*(j_max+2) + (j+1)]);
+    double stencil4 = fabs(stencil2) * 0.5 * (v[i*(j_max+2) + (j-1)] - v[i*(j_max+2) + j]);
+
+    return 1/delta_y * (stencil1*stencil1 - stencil2*stencil2) + gamma / delta_y * (stencil3 - stencil4);
+}
+
+inline double duv_dx_gpu(double* u, double* v, int i, int j, int j_max, double delta_x, double gamma) {
+    double stencil1 = 0.5 * (u[i*(j_max+2) + j] + u[i*(j_max+2) + (j+1)]);
+    double stencil2 = 0.5 * (u[(i-1)*(j_max+2) + j] + u[(i-1)*(j_max+2) + (j+1)]);
+
+    double stencil3 = stencil1 * 0.5 * (v[i*(j_max+2) + j] + v[(i+1)*(j_max+2) + j]);
+    double stencil4 = stencil2 * 0.5 * (v[(i-1)*(j_max+2) + j] + v[i*(j_max+2) + j]);
+
+    double stencil5 = fabs(stencil1) * 0.5 * (v[i*(j_max+2) + j] - v[(i+1)*(j_max+2) + j]);
+    double stencil6 = fabs(stencil2) * 0.5 * (v[(i-1)*(j_max+2) + j] - v[i*(j_max+2) + j]);
+
+    return 1/delta_x * (stencil3 - stencil4) + gamma / delta_x * (stencil5 - stencil6);
+}
+
+inline double d2u_dx2_gpu(double* u, int i, int j, int j_max, double delta_x) {
+    return (u[(i+1)*(j_max+2) + j] - 2 * u[i*(j_max+2) + j] + u[(i-1)*(j_max+2) + j]) / (delta_x*delta_x);
+}
+
+inline double d2u_dy2_gpu(double* u, int i, int j, int j_max, double delta_y) {
+    return (u[i*(j_max+2) + (j+1)] - 2 * u[i*(j_max+2) + j] + u[i*(j_max+2) + (j-1)]) / (delta_y*delta_y);
+}
+
+inline double d2v_dx2_gpu(double* v, int i, int j, int j_max, double delta_x) {
+    return (v[(i+1)*(j_max+2) + j] - 2 * v[i*(j_max+2) + j] + v[(i-1)*(j_max+2) + j]) / (delta_x*delta_x);
+}
+
+inline double d2v_dy2_gpu(double* v, int i, int j, int j_max, double delta_y) {
+    return (v[i*(j_max+2) + (j+1)] - 2 * v[i*(j_max+2) + j] + v[i*(j_max+2) + (j-1)]) / (delta_y*delta_y);
+}
+
+inline double dp_dx_gpu(double* p, int i, int j, int j_max, double delta_x) {
+    return (p[(i+1)*(j_max+2) + j] - p[(i-1)*(j_max+2) + j]) / (2.0 * delta_x);
+}
+
+inline double dp_dy_gpu(double* p, int i, int j, int j_max, double delta_y) {
+    return (p[i*(j_max+2) + (j+1)] - p[i*(j_max+2) + (j-1)]) / (2.0 * delta_y);
+}
+#pragma omp end declare target
+
 int SOR_GPU(double** p, int i_max, int j_max, double delta_x, double delta_y, double** res, double** RHS, 
             double omega, double eps, int max_it) {
     // Constantes úteis
@@ -154,6 +230,316 @@ int SOR_GPU(double** p, int i_max, int j_max, double delta_x, double delta_y, do
     
     return result;
 }
+
+
+double max_mat_gpu(int i_max, int j_max, double** m) {
+    // Alocar array contíguo para GPU
+    double* m_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    
+    // Copiar dados para array contíguo
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i <= i_max+1; i++) {
+        for (int j = 0; j <= j_max+1; j++) {
+            m_flat[i*(j_max+2) + j] = m[i][j];
+        }
+    }
+    
+    double max_val = 0.0;
+    
+    // Computação na GPU
+    #pragma omp target data map(to: m_flat[0:(i_max+2)*(j_max+2)]) map(tofrom: max_val)
+    {
+        #pragma omp target teams distribute parallel for collapse(2) reduction(max:max_val)
+        for (int i = 1; i <= i_max; i++) {
+            for (int j = 1; j <= j_max; j++) {
+                if (fabs(m_flat[i*(j_max+2) + j]) > max_val) {
+                    max_val = fabs(m_flat[i*(j_max+2) + j]);
+                }
+            }
+        }
+    }
+    
+    free(m_flat);
+    return max_val;
+}
+
+
+double n_min_gpu(int n, ...) {
+    va_list args;
+    va_start(args, n);
+    double min_val = va_arg(args, double);
+    
+    for (int i = 1; i < n; i++) {
+        double val = va_arg(args, double);
+        if (val < min_val) {
+            min_val = val;
+        }
+    }
+    
+    va_end(args);
+    return min_val;
+}
+
+
+int FG_GPU(double** F, double** G, double** u, double** v, int i_max, int j_max, 
+           double Re, double g_x, double g_y, double delta_t, double delta_x, 
+           double delta_y, double gamma) {
+    
+    // Alocar arrays contíguos
+    double* F_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    double* G_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    double* u_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    double* v_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    
+    // Copiar dados para arrays contíguos
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i <= i_max+1; i++) {
+        for (int j = 0; j <= j_max+1; j++) {
+            u_flat[i*(j_max+2) + j] = u[i][j];
+            v_flat[i*(j_max+2) + j] = v[i][j];
+            if (i <= i_max && j <= j_max) {
+                F_flat[i*(j_max+2) + j] = F[i][j];
+                G_flat[i*(j_max+2) + j] = G[i][j];
+            }
+        }
+    }
+    
+    // Computação na GPU
+    #pragma omp target data map(to: u_flat[0:(i_max+2)*(j_max+2)], v_flat[0:(i_max+2)*(j_max+2)], i_max, j_max, Re, g_x, g_y, delta_t, delta_x, delta_y, gamma) \
+                         map(tofrom: F_flat[0:(i_max+2)*(j_max+2)], G_flat[0:(i_max+2)*(j_max+2)])
+    {
+        #pragma omp target teams distribute parallel for collapse(2)
+        for (int i = 1; i <= i_max; i++) {
+            for (int j = 1; j <= j_max; j++) {
+                if (i <= i_max - 1) {
+                    F_flat[i*(j_max+2) + j] = u_flat[i*(j_max+2) + j] +
+                                delta_t * (1.0/Re * (d2u_dx2_gpu(u_flat, i, j, j_max, delta_x) + 
+                                                     d2u_dy2_gpu(u_flat, i, j, j_max, delta_y)) -
+                                          du2_dx_gpu(u_flat, v_flat, i, j, j_max, delta_x, gamma) -
+                                          duv_dy_gpu(u_flat, v_flat, i, j, j_max, delta_y, gamma) +
+                                          g_x);
+                }
+
+                if (j <= j_max - 1) {
+                    G_flat[i*(j_max+2) + j] = v_flat[i*(j_max+2) + j] +
+                                delta_t * (1.0/Re * (d2v_dx2_gpu(v_flat, i, j, j_max, delta_x) + 
+                                                     d2v_dy2_gpu(v_flat, i, j, j_max, delta_y)) -
+                                          duv_dx_gpu(u_flat, v_flat, i, j, j_max, delta_x, gamma) -
+                                          dv2_dy_gpu(u_flat, v_flat, i, j, j_max, delta_y, gamma) +
+                                          g_y);
+                }
+            }
+        }
+    }
+    
+    // Copiar dados de volta
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i <= i_max+1; i++) {
+        for (int j = 0; j <= j_max+1; j++) {
+            if (i <= i_max && j <= j_max) {
+                F[i][j] = F_flat[i*(j_max+2) + j];
+                G[i][j] = G_flat[i*(j_max+2) + j];
+            }
+        }
+    }
+    
+    // Liberar memória
+    free(F_flat);
+    free(G_flat);
+    free(u_flat);
+    free(v_flat);
+    
+    return 0;
+}
+
+
+void calculate_RHS_GPU(double** RHS, double** F, double** G, int i_max, int j_max, double delta_t, double delta_x, double delta_y) {
+    // Alocar arrays contíguos
+    double* RHS_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    double* F_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    double* G_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    
+    // Copiar dados para arrays contíguos
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i <= i_max+1; i++) {
+        for (int j = 0; j <= j_max+1; j++) {
+            if (i <= i_max && j <= j_max) {
+                F_flat[i*(j_max+2) + j] = F[i][j];
+                G_flat[i*(j_max+2) + j] = G[i][j];
+                RHS_flat[i*(j_max+2) + j] = RHS[i][j];
+            }
+        }
+    }
+    
+    // Computação na GPU
+    #pragma omp target data map(to: F_flat[0:(i_max+2)*(j_max+2)], G_flat[0:(i_max+2)*(j_max+2)], i_max, j_max, delta_t, delta_x, delta_y) \
+                         map(tofrom: RHS_flat[0:(i_max+2)*(j_max+2)])
+    {
+        #pragma omp target teams distribute parallel for collapse(2)
+        for (int i = 1; i <= i_max; i++) {
+            for (int j = 1; j <= j_max; j++) {
+                RHS_flat[i*(j_max+2) + j] = 1.0 / delta_t * (
+                    (F_flat[i*(j_max+2) + j] - F_flat[(i-1)*(j_max+2) + j]) / delta_x + 
+                    (G_flat[i*(j_max+2) + j] - G_flat[i*(j_max+2) + (j-1)]) / delta_y
+                );
+            }
+        }
+    }
+    
+    // Copiar dados de volta
+    #pragma omp parallel for collapse(2)
+    for (int i = 1; i <= i_max; i++) {
+        for (int j = 1; j <= j_max; j++) {
+            RHS[i][j] = RHS_flat[i*(j_max+2) + j];
+        }
+    }
+    
+    // Liberar memória
+    free(RHS_flat);
+    free(F_flat);
+    free(G_flat);
+}
+
+
+void update_velocities_GPU(double** u, double** v, double** F, double** G, double** p, 
+                          int i_max, int j_max, double delta_t, double delta_x, double delta_y) {
+    // Alocar arrays contíguos
+    double* u_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    double* v_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    double* F_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    double* G_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    double* p_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    
+    // Copiar dados para arrays contíguos
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i <= i_max+1; i++) {
+        for (int j = 0; j <= j_max+1; j++) {
+            u_flat[i*(j_max+2) + j] = u[i][j];
+            v_flat[i*(j_max+2) + j] = v[i][j];
+            p_flat[i*(j_max+2) + j] = p[i][j];
+            if (i <= i_max && j <= j_max) {
+                F_flat[i*(j_max+2) + j] = F[i][j];
+                G_flat[i*(j_max+2) + j] = G[i][j];
+            }
+        }
+    }
+    
+        
+    // Computação na GPU
+    #pragma omp target data map(to: F_flat[0:(i_max+2)*(j_max+2)], G_flat[0:(i_max+2)*(j_max+2)], p_flat[0:(i_max+2)*(j_max+2)], i_max, j_max, delta_t, delta_x, delta_y) \
+                         map(tofrom: u_flat[0:(i_max+2)*(j_max+2)], v_flat[0:(i_max+2)*(j_max+2)])
+    {
+        #pragma omp target teams distribute parallel for collapse(2)
+        for (int i = 1; i <= i_max; i++) {
+            for (int j = 1; j <= j_max; j++) {
+                if (i <= i_max - 1) {
+                    u_flat[i*(j_max+2) + j] = F_flat[i*(j_max+2) + j] - 
+                                             delta_t * dp_dx_gpu(p_flat, i, j, j_max, delta_x);
+                }
+                
+                if (j <= j_max - 1) {
+                    v_flat[i*(j_max+2) + j] = G_flat[i*(j_max+2) + j] - 
+                                             delta_t * dp_dy_gpu(p_flat, i, j, j_max, delta_y);
+                }
+            }
+        }
+    }
+    
+    // Copiar dados de volta
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i <= i_max+1; i++) {
+        for (int j = 0; j <= j_max+1; j++) {
+            u[i][j] = u_flat[i*(j_max+2) + j];
+            v[i][j] = v_flat[i*(j_max+2) + j];
+        }
+    }
+    
+    // Liberar memória
+    free(u_flat);
+    free(v_flat);
+    free(F_flat);
+    free(G_flat);
+    free(p_flat);
+}
+
+
+
+int set_inflow_GPU(int i_max, int j_max, double** u, double** v, int side, double u_fix, double v_fix) {
+    // Alocar arrays contíguos
+    double* u_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    double* v_flat = (double*)malloc((i_max+2) * (j_max+2) * sizeof(double));
+    
+    // Copiar dados para arrays contíguos
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i <= i_max+1; i++) {
+        for (int j = 0; j <= j_max+1; j++) {
+            u_flat[i*(j_max+2) + j] = u[i][j];
+            v_flat[i*(j_max+2) + j] = v[i][j];
+        }
+    }
+    
+    // Computação na GPU - usar um kernel para cada lado
+    #pragma omp target data map(tofrom: u_flat[0:(i_max+2)*(j_max+2)], v_flat[0:(i_max+2)*(j_max+2)]) \
+                         map(to: i_max, j_max, side, u_fix, v_fix)
+    {
+        switch(side) {
+            case 0: // TOP
+                #pragma omp target teams distribute parallel for
+                for (int i = 1; i <= i_max; i++) {
+                    v_flat[i*(j_max+2) + j_max] = v_fix;
+                    u_flat[i*(j_max+2) + (j_max+1)] = 2 * u_fix - u_flat[i*(j_max+2) + j_max];
+                }
+                break;
+                
+            case 1: // BOTTOM
+                #pragma omp target teams distribute parallel for
+                for (int i = 1; i <= i_max; i++) {
+                    v_flat[i*(j_max+2) + 0] = v_fix;
+                    u_flat[i*(j_max+2) + 0] = 2 * u_fix - u_flat[i*(j_max+2) + 1];
+                }
+                break;
+                
+            case 2: // LEFT
+                #pragma omp target teams distribute parallel for
+                for (int j = 1; j <= j_max; j++) {
+                    u_flat[0*(j_max+2) + j] = u_fix;
+                    v_flat[0*(j_max+2) + j] = 2 * v_fix - v_flat[1*(j_max+2) + j];
+                }
+                break;
+                
+            case 3: // RIGHT
+                #pragma omp target teams distribute parallel for
+                for (int j = 1; j <= j_max; j++) {
+                    u_flat[i_max*(j_max+2) + j] = u_fix;
+                    v_flat[(i_max+1)*(j_max+2) + j] = 2 * v_fix - v_flat[i_max*(j_max+2) + j];
+                }
+                break;
+        }
+    }
+    
+    // Copiar dados de volta
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i <= i_max+1; i++) {
+        for (int j = 0; j <= j_max+1; j++) {
+            u[i][j] = u_flat[i*(j_max+2) + j];
+            v[i][j] = v_flat[i*(j_max+2) + j];
+        }
+    }
+    
+    // Liberar memória
+    free(u_flat);
+    free(v_flat);
+    
+    return 0;
+}
+
+
+int set_noslip_GPU(int i_max, int j_max, double** u, double** v, int side) {
+    return set_inflow_GPU(i_max, j_max, u, v, side, 0.0, 0.0);
+}
+
+
+
 /**
  * @brief Main function.
  * 
@@ -217,63 +603,43 @@ int main(int argc, char* argv[])
     clock_t start = clock();
     double time_sor = 0.0;
     while (t < T) {
-
-    	// Adaptive stepsize and weight factor for Donor-Cell
-        double u_max = max_mat(i_max, j_max, u);
-        double v_max = max_mat(i_max, j_max, v);
-    	delta_t = tau * n_min(3, Re / 2.0 / ( 1.0 / delta_x / delta_x + 1.0 / delta_y / delta_y ), delta_x / fabs(u_max), delta_y / fabs(v_max));
+        // Adaptive stepsize and weight factor for Donor-Cell
+        double u_max = max_mat_gpu(i_max, j_max, u);
+        double v_max = max_mat_gpu(i_max, j_max, v);
+        delta_t = tau * n_min_gpu(3, Re / 2.0 / (1.0 / delta_x / delta_x + 1.0 / delta_y / delta_y), 
+                           delta_x / fabs(u_max), delta_y / fabs(v_max));
         gamma = fmax(u_max * delta_t / delta_x, v_max * delta_t / delta_y);
 
         // Set boundary conditions.
         if (problem == 1) {
-            set_noslip(i_max, j_max, u, v, LEFT);
-            set_noslip(i_max, j_max, u, v, RIGHT);
-            set_noslip(i_max, j_max, u, v, BOTTOM);
-            set_inflow(i_max, j_max, u, v, TOP, 1.0, 0.0);
+            set_noslip_GPU(i_max, j_max, u, v, 2); // LEFT
+            set_noslip_GPU(i_max, j_max, u, v, 3); // RIGHT
+            set_noslip_GPU(i_max, j_max, u, v, 1); // BOTTOM
+            set_inflow_GPU(i_max, j_max, u, v, 0, 1.0, 0.0); // TOP
         } else if (problem == 2) {
-            set_noslip(i_max, j_max, u, v, LEFT);
-            set_noslip(i_max, j_max, u, v, RIGHT);
-            set_noslip(i_max, j_max, u, v, BOTTOM);
-            set_inflow(i_max, j_max, u, v, TOP, sin(f*t), 0.0);           
+            set_noslip_GPU(i_max, j_max, u, v, 2); // LEFT
+            set_noslip_GPU(i_max, j_max, u, v, 3); // RIGHT
+            set_noslip_GPU(i_max, j_max, u, v, 1); // BOTTOM
+            set_inflow_GPU(i_max, j_max, u, v, 0, sin(f*t), 0.0); // TOP        
         } else {
             printf("Unknown probem type (see parameters.txt).\n");
             exit(EXIT_FAILURE);
         }
 
-
         // Calculate F and G.
-        FG(F, G, u, v, i_max, j_max, Re, g_x, g_y, delta_t, delta_x, delta_y, gamma);
-
+        FG_GPU(F, G, u, v, i_max, j_max, Re, g_x, g_y, delta_t, delta_x, delta_y, gamma);
 
         // RHS of Poisson equation.
-        for (i = 1; i <= i_max; i++ ) {
-            for (j = 1; j <= j_max; j++) {
-                RHS[i][j] = 1.0 / delta_t * ((F[i][j] - F[i-1][j])/delta_x + (G[i][j] - G[i][j-1])/delta_y);
-            }
-        }
-        //clock_t start_UVA = clock();
+        calculate_RHS_GPU(RHS, F, G, i_max, j_max, delta_t, delta_x, delta_y);
+        
+        // Solve Poisson equation for pressure
         clock_t start_sor = clock();
         SOR_GPU(p, i_max, j_max, delta_x, delta_y, res, RHS, omega, epsilon, max_it);
         clock_t end_sor = clock();
         time_sor += (double)(end_sor - start_sor) / CLOCKS_PER_SEC;
-        //clock_t end_UVA = clock();
-        //double time_UVA = (double)(end_UVA - start_UVA) / CLOCKS_PER_SEC;
-        //fprintf(stderr, "SOR UVA time: %.6f\n", time_UVA);
 
         // Update velocities.
-        for (i = 1; i <= i_max; i++ ) {
-            for (j = 1; j <= j_max; j++) {
-                if (i <= i_max - 1) u[i][j] = F[i][j] - delta_t * dp_dx(p, i, j, delta_x);
-                if (j <= j_max - 1) v[i][j] = G[i][j] - delta_t * dp_dy(p, i, j, delta_y);
-            }
-        }
-        // Print to file every ..th step.
-        // if (n % n_print == 0) {
-        //     char out_prefix[12];
-        //     sprintf(out_prefix, "out/%d", n_out);
-        //     output(i_max, j_max, u, v, p, t, a, b, out_prefix);
-        //     n_out++;
-        // }
+        update_velocities_GPU(u, v, F, G, p, i_max, j_max, delta_t, delta_x, delta_y);
 
         t += delta_t;
         n++;
